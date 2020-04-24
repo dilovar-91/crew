659f5bcb240618c2f90ec14f2c7607fc8271e216
Dilovar Azimov <dilovar09@gmail.com>
2020-04-25 02:34:24 +0300

    changes dd

--- app/Http/Controllers/SeamenController.php
+++ app/Http/Controllers/SeamenController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use CloudConvert\Laravel\Facades\CloudConvert;
+use CloudConvert;
 
 class SeamenController extends Controller
 {
