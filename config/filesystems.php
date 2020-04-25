1786ede6a97b8ec8de516d69ec2f6fdb08dbc237
Dilovar Azimov <dilovar09@gmail.com>
2020-04-25 14:05:21 +0300

    upload

--- config/filesystems.php
+++ config/filesystems.php
@@ -56,7 +56,7 @@ return [
         ],*/
         'public' => [
             'driver' => 'local',
-            'root'   => public_path() . '/uploads',
+            'root'   => public_path() . '/',
             'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
