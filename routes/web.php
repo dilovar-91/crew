bd0684aa029b676903b4337f8a96975d4bca514b
Dilovar Azimov <dilovar09@gmail.com>
2020-04-25 15:41:21 +0300

    storage route

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,19 @@ Route::view('/landing', 'landing');
 
 Route::post('/seamen/video/send', 'SeamenController@videoSend')->name('videoPost');
 
+Route::get('storage/{filename}', function ($filename)
+{
+    $path = storage_path('public/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
