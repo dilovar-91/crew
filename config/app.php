d99019b7480a581f9df388ade602d72e49f2678a
Dilovar Azimov <dilovar09@gmail.com>
2020-04-25 13:38:20 +0300

    removed

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Pbmedia\LaravelFFMpeg\FFMpegServiceProvider::class,
+        
 
     ],
 
