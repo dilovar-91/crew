3123e8caf97fcfb9f085f7dab9c37cb13467de56
Dilovar Azimov <dilovar09@gmail.com>
2020-04-29 01:45:07 +0300

    answer

--- app/Jobs/ProcessVideoConvert.php
+++ app/Jobs/ProcessVideoConvert.php
@@ -15,6 +15,7 @@ class ProcessVideoConvert implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $answer;
     /**
      * Create a new job instance.
      *
