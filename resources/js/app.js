dcb6693ae889229b41d873f6eac0ceaf0c6d3160
Dilovar Azimov <dilovar09@gmail.com>
2020-04-28 15:47:03 +0300

    IMPORTED

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,6 +11,10 @@ import MediaStreamRecorder   from "msr";
 //Vue.use(RecordRTC);
 Vue.use(MediaStreamRecorder);
 
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+Vue.use(VueSweetalert2);
+
 
 
 /**
