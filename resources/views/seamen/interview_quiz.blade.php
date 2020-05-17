b477648ece42bc1d7023b1cdc561c412dd1bfd81
Dilovar Azimov <dilovar09@gmail.com>
2020-05-18 00:50:08 +0300

    removed id from quiz page

--- resources/views/seamen/interview_quiz.blade.php
+++ resources/views/seamen/interview_quiz.blade.php
@@ -7,7 +7,7 @@
         <div class="content content-full">
             <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
                 <h1 class="flex-sm-fill h3 my-2">
-                Quiz for <span class="text-primary">"{{$interview->title}}" {{$interview->invite['user_id']}}</span> 
+                Quiz for <span class="text-primary">"{{$interview->title}}"</span> 
                 </h1>
                 <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-alt">
